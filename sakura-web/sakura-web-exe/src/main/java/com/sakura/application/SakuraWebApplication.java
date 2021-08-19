c1cf6887136ed338356e35b5ec15eb15a374852f
毕英豪 <724500573@qq.com>
2021-08-19 10:32:28 +0800

    11

--- sakura-web/sakura-web-exe/src/main/java/com/sakura/application/SakuraWebApplication.java
+++ sakura-web/sakura-web-exe/src/main/java/com/sakura/application/SakuraWebApplication.java
@@ -5,17 +5,15 @@ import com.sakura.farme.annotation.EnableSakuraMybatis;
 import com.sakura.web.annotation.EnableRedisSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author : bi
  * @since : 2021年06月24日
  */
 @EnableRedis
-@EnableSakuraMybatis
 @EnableRedisSession
-@SpringBootApplication
-@ComponentScan({"com.sakura"})
+@EnableSakuraMybatis
+@SpringBootApplication(scanBasePackages = {"com.sakura"})
 public class SakuraWebApplication {
 
     public static void main(String[] args) {
